e876c06dfc3ee9a3cdbcdc1576fa5d952fc85bf6
Dongwoo Kang <alsdn1360@ajou.ac.kr>
2024-07-06 18:15:35 +0900

    Design: Change custom color
    `background`, `lightGray` 색상 변경

--- lib/themes/custom_color.dart
+++ lib/themes/custom_color.dart
@@ -5,11 +5,11 @@ const Color transparent = Colors.transparent;
 const Color black = Color(0xFF222222);
 const Color darkGray = Color(0xFF69696C);
 const Color gray = Color(0xFFA8A8AB);
-const Color lightGray = Color(0xFFD2D2D6);
+const Color lightGray = Color(0xFFD2D3D7);
 const Color white = Color(0xFFFCFCFC);
 
 /// Etc
-const Color background = Color(0xFFF2F2F6);
+const Color background = Color(0xFFF2F3F7);
 const Color red = Color(0xFFD32F2F);
 const Color blue = Color(0xFF1976D2);
 const Color orange = Color(0xFFFB8C00);
